8e8512c864a809175b45bf25f5325e2bcbc6821c
MarinaChizh <47003040+MarinaChizh@users.noreply.github.com>
2019-04-23 16:57:56 +0300

    23/04/2019

---
+++ OOP/primer_dz.php
@@ -0,0 +1,56 @@
+﻿<?php
+class Form{
+
+private function getAttribute($attr){
+
+$str = "";
+foreach ($attr as $key => $item) {
+$str .= $key ."=".$item;
+}
+return $str;
+}
+
+public function input($attr){
+
+$attr = $this->getAttribute($attr);
+return "<input $attr><br>\n<br>";
+}
+
+public function password($attr){
+
+$attr = $this->getAttribute($attr);
+return "<input $attr>\n<br><br>";
+}
+
+public function textarea($attr) {
+$attr = $this->getAttribute($attr);
+return '<textarea ' . $attr . '></textarea><br><br>';
+}
+
+public function submit($attr =[]){
+
+$attr = $this->getAttribute($attr);
+return "<input type='submit'".$attr.">";
+}
+
+public function open($attr){
+
+$attr = $this->getAttribute($attr);
+return "<form" .$attr.">\n<br>";
+}
+
+public function close(){
+
+return "</form>";
+}
+}
+
+$form = new Form();
+echo $form->open(['action' => '', 'method' => 'post']);
+echo $form->input(['type' => 'text', 'name' => 'login']);
+echo $form->password(['type' => 'password', 'name' => 'pass']);
+echo $form->textarea(['name' => 'text', 'cols' => 100, 'rows' => 5]);
+echo $form->submit();
+echo $form->close();
+
+?>
