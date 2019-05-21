e6a61878a26d499e46739942377d9a8660b15b69
Marina <marychizh@yandex.ru>
2019-05-21 20:54:23 +0300

    21/05/2019

---
+++ OOP/igra/comp_igra.php
@@ -0,0 +1,29 @@
+<?php
+require_once "Tic_Tac_Toe_Para.php";
+class Igra extends Tic_Tac_Toe
+{
+    function put_cross(){
+$size = count($this->array);
+$i = round(rand(0, ($size-1)));
+$j = round(rand(0, ($size-1)));
+if ($this->in_matrix($i, $j) && $this->is_empty($i, $j) && $this->check_move('cross')){
+    $this->array[$i][$j] = 'X';
+    $this->turn_move();
+}
+    }
+function put_circle(){
+    function put_cross(){
+        $size = count($this->array);
+        $i = round(rand(0, ($size-1)));
+        $j = round(rand(0, ($size-1)));
+        if ($this->in_matrix($i, $j) && $this->is_empty($i, $j) && $this->check_move('circle')){
+            $this->array[$i][$j] = 'O';
+            $this->turn_move();
+        }
+}
+
+
+}
+}
+    
+
---
+++ OOP/igra/test.php
@@ -0,0 +1,116 @@
+<?
+
+require_once "Tic_Tac_Toe_Para.php";
+require_once "comp_igra.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<?
+// $obj = new Tic_Tac_Toe (3);
+$obj = new Igra (3);
+$obj -> put_cross();
+$obj->show();
+$obj -> put_circle();
+$obj->show();
+echo $obj->check_winner();
+// столбец Х
+// $obj->show();
+// $obj->put_circle(3,1);
+// $obj->show();
+// $obj->put_cross(1,2);
+// $obj->show();
+// $obj->put_circle(1,2);
+// $obj->show();
+// $obj->put_cross(0,0);
+// $obj->show();
+// $obj->put_circle(0,1);
+// $obj->show();
+// $obj->put_cross(1,0);
+// $obj->show();
+// $obj->put_circle(2,1);
+// $obj->show();
+// $obj->put_cross(2,0);
+// $obj->show();
+// echo $obj->check_winner();
+
+// диагональ О
+// $obj->show();
+// $obj->put_circle(3,1);
+// $obj->show();
+// $obj->put_cross(1,2);
+// $obj->show();
+// $obj->put_circle(1,2);
+// $obj->show();
+// $obj->put_cross(0,1);
+// $obj->show();
+// $obj->put_circle(0,2);
+// $obj->show();
+// $obj->put_cross(1,0);
+// $obj->show();
+// $obj->put_circle(1,1);
+// $obj->show();
+// $obj->put_cross(2,1);
+// $obj->show();
+// $obj->put_circle(2,0);
+// $obj->show();
+// echo $obj->check_winner();
+
+// горизонт О
+// $obj->show();
+// $obj->put_circle(3,1);
+// $obj->show();
+// $obj->put_cross(1,2);
+// $obj->show();
+// $obj->put_circle(1,2);
+// $obj->show();
+// $obj->put_cross(1,1);
+// $obj->show();
+// $obj->put_circle(0,0);
+// $obj->show();
+// $obj->put_cross(1,0);
+// $obj->show();
+// $obj->put_circle(0,1);
+// $obj->show();
+// $obj->put_cross(2,1);
+// $obj->show();
+// $obj->put_circle(0,2);
+// $obj->show();
+// echo $obj->check_winner();
+
+
+// диагональ X
+// $obj->show();
+// $obj->put_circle(3,1);
+// $obj->show();
+// $obj->put_cross(1,2);
+// $obj->show();
+// $obj->put_circle(1,2);
+// $obj->show();
+// $obj->put_cross(0,2);
+// $obj->show();
+// $obj->put_circle(0,0);
+// $obj->show();
+// $obj->put_cross(1,1);
+// $obj->show();
+// $obj->put_circle(0,1);
+// $obj->show();
+// $obj->put_cross(2,0);
+// $obj->show();
+// $obj->put_circle(0,2);
+// $obj->show();
+// echo $obj->check_winner();
+
+
